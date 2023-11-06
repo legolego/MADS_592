6fff324d320512381b328758a8e3011b80924c4e
Oleg N <legolego@users.noreply.github.com>
2023-11-06 11:57:10 -0500

    Update streamlit_test.py
    Got rid of some unneeded text

--- streamlit_test.py
+++ streamlit_test.py
@@ -8,21 +8,6 @@ c = alt.Chart(df).mark_circle().encode(x='a', y='b', size='c',
                                        color='c')
 st.altair_chart(c) # , width=-1
 
-st.sidebar.title("About")
-
-<<<<<<< Updated upstream
-st.sidebar.title("Train Neural Network")
-if st.sidebar.button('Train CNN'):
-    print("Train NN")
-=======
-st.sidebar.info(
-    "Hi This still is a demo application written to help you understand Streamlit. The application identifies the animal in the picture. It was built using a Convolution Neural Network (CNN).")
-
->>>>>>> Stashed changes
-
-st.sidebar.title("Sidebar title")
-if st.sidebar.button('Im a button'):
-    st.write("You pressed a button!.")
 
 st.sidebar.title("Predict New Images")
 
